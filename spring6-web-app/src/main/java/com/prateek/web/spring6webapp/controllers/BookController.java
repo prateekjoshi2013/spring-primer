5b9849af9ad7d025bf9bea50704af7129d11ba74
prateek <prateekjoshi2013@gmail.com>
2024-07-16 06:50:57 +0000

    Added BookController

---
+++ spring6-web-app/src/main/java/com/prateek/web/spring6webapp/controllers/BookController.java
@@ -0,0 +1,24 @@
+package com.prateek.web.spring6webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.prateek.web.spring6webapp.services.BookService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "books";
+    }
+
+}
