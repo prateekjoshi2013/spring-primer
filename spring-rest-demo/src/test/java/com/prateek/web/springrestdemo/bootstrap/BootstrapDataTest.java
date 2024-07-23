0c0cbcf162f0e8b1501a316deb1e7b56a2481fa6
prateek <prateekjoshi2013@gmail.com>
2024-07-23 05:37:29 +0000

    fixed BootstrapDataTest by injecting CSV mapper and service @Import is needed because JPATest does not import other spring beans like services controllers etc

--- spring-rest-demo/src/test/java/com/prateek/web/springrestdemo/bootstrap/BootstrapDataTest.java
+++ spring-rest-demo/src/test/java/com/prateek/web/springrestdemo/bootstrap/BootstrapDataTest.java
@@ -6,13 +6,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
+import com.prateek.web.springrestdemo.mappers.BeerCsvMapper;
+import com.prateek.web.springrestdemo.mappers.BeerCsvMapperImpl;
 import com.prateek.web.springrestdemo.repositories.BeerRepository;
 import com.prateek.web.springrestdemo.repositories.CustomerRepository;
+import com.prateek.web.springrestdemo.services.BeerCsvService;
+import com.prateek.web.springrestdemo.services.BeerCsvServiceImpl;
 
 import lombok.SneakyThrows;
 
 @DataJpaTest
+@Import({ BeerCsvServiceImpl.class, BeerCsvMapperImpl.class })
 public class BootstrapDataTest {
 
     @Autowired
@@ -21,18 +27,24 @@ public class BootstrapDataTest {
     @Autowired
     private BeerRepository beerRepository;
 
+    @Autowired
+    private BeerCsvService beerCsvServiceImpl;
+
+    @Autowired
+    private BeerCsvMapper beerCsvMapperImpl;
+
     private BootstrapData bootstrapData;
 
     @BeforeEach
     void setUp() {
-        bootstrapData = new BootstrapData(beerRepository, customerRepository);
+        bootstrapData = new BootstrapData(beerRepository, customerRepository, beerCsvServiceImpl, beerCsvMapperImpl);
     }
 
     @SneakyThrows
     @Test
     void testRun() {
         bootstrapData.run();
-        assertEquals(beerRepository.count(), 3l);
+        assertEquals(beerRepository.count(), 2410l);
         assertEquals(customerRepository.count(), 3l);
     }
 }
