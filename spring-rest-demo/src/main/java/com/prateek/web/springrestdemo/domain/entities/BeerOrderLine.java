6496e58409b572e27a649afac1473a933a0c4267
prateek <prateekjoshi2013@gmail.com>
2024-07-24 16:57:35 +0000

    Added BeerOrder & BeerOrderLine ent without rels Added a migration script to change the version columns types from bigint to int

---
+++ spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/domain/entities/BeerOrderLine.java
@@ -0,0 +1,55 @@
+package com.prateek.web.springrestdemo.domain.entities;
+
+import java.math.BigInteger;
+import java.security.Timestamp;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.annotations.UuidGenerator.Style;
+import org.hibernate.type.SqlTypes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerOrderLine {
+    @Id
+    @UuidGenerator(style = Style.RANDOM)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Integer version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    @UpdateTimestamp
+    private LocalDateTime lastModifiedDate;
+
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    private Integer orderQuantity = 0;
+    private Integer quantityAllocated = 0;
+
+}
---
+++ spring-rest-demo/src/main/resources/db/migration/mysql/V4__change_version_column_beer_order_and_beer_order_line.sql
@@ -0,0 +1,2 @@
+ALTER TABLE beer_order MODIFY version INTEGER;
+ALTER TABLE beer_order_line MODIFY version INTEGER;
