331fff68beee0a58ab8df62bce5cf7848e7d60f9
prateek <prateekjoshi2013@gmail.com>
2024-07-24 19:22:49 +0000

    Cleaned up BeerOrder and BeerOrderLine

--- spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/domain/entities/BeerOrder.java
+++ spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/domain/entities/BeerOrder.java
@@ -21,6 +21,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -28,6 +29,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BeerOrder {
--- spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/domain/entities/BeerOrderLine.java
+++ spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/domain/entities/BeerOrderLine.java
@@ -1,12 +1,9 @@
 package com.prateek.web.springrestdemo.domain.entities;
 
-import java.math.BigInteger;
-import java.security.Timestamp;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.JdbcType;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
@@ -16,7 +13,6 @@ import org.hibernate.type.SqlTypes;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
