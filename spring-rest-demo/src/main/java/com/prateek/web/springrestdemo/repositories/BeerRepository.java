54c3f637655deaea059f3f367dab7654fcd122f6
prateek <prateekjoshi2013@gmail.com>
2024-07-24 05:30:33 +0000

    refactor beerJPArepository use pagination

--- spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/repositories/BeerRepository.java
+++ spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/repositories/BeerRepository.java
@@ -3,15 +3,17 @@ package com.prateek.web.springrestdemo.repositories;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.prateek.web.springrestdemo.domain.entities.Beer;
 import com.prateek.web.springrestdemo.model.BeerStyle;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
-    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
+    Page<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName, Pageable pageable);
 
-    List<Beer> findAllByBeerStyle(BeerStyle beerStyle);
+    Page<Beer> findAllByBeerStyle(BeerStyle beerStyle, Pageable pageable);
 
-    List<Beer> findAllByBeerNameIsLikeIgnoreCaseAndBeerStyle(String beerName, BeerStyle beerStyle);
+    Page<Beer> findAllByBeerNameIsLikeIgnoreCaseAndBeerStyle(String beerName, BeerStyle beerStyle, Pageable pageable);
 }
