4ecb174df00f8e3f4693c2feb9d1c1f714deeb3d
prateek <prateekjoshi2013@gmail.com>
2024-07-24 02:05:11 +0000

    Refactor BeerJPA repo with autogen jpa method

--- spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/repositories/BeerRepository.java
+++ spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/repositories/BeerRepository.java
@@ -1,11 +1,15 @@
 package com.prateek.web.springrestdemo.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.prateek.web.springrestdemo.domain.entities.Beer;
+import com.prateek.web.springrestdemo.model.BeerStyle;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
 
+    List<Beer> findAllByBeerStyle(BeerStyle beerStyle);
 }
--- spring-rest-demo/src/test/java/com/prateek/web/springrestdemo/repositories/BeerRepositoryTest.java
+++ spring-rest-demo/src/test/java/com/prateek/web/springrestdemo/repositories/BeerRepositoryTest.java
@@ -4,20 +4,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
+import com.prateek.web.springrestdemo.bootstrap.BootstrapData;
 import com.prateek.web.springrestdemo.domain.entities.Beer;
+import com.prateek.web.springrestdemo.mappers.BeerCsvMapperImpl;
 import com.prateek.web.springrestdemo.model.BeerStyle;
+import com.prateek.web.springrestdemo.services.BeerCsvServiceImpl;
 
 import jakarta.validation.ConstraintViolationException;
 
 @DataJpaTest
+@Import({ BootstrapData.class, BeerCsvServiceImpl.class, BeerCsvMapperImpl.class })
 public class BeerRepositoryTest {
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testGetBeerListByName() {
+        List<Beer> list = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+        assertThat(list.size()).isEqualTo(336);
+    }
 
     @Test
     void testSaveBeer() {
