87f84b5aff5ac65937d49044a7d292f8246925a1
prateek <prateekjoshi2013@gmail.com>
2024-07-28 13:18:33 -0700

    Set up beerclient using restemplate added test for testing the endpoint we created for spring-rest-demo

--- spring-6-rest-template-main/src/main/java/guru/springframework/spring6resttemplate/clients/BeerClientImpl.java
+++ spring-6-rest-template-main/src/main/java/guru/springframework/spring6resttemplate/clients/BeerClientImpl.java
@@ -1,15 +1,27 @@
 package guru.springframework.spring6resttemplate.clients;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
 
+@Service
+@RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
 
     @Override
     public Page<BeerDTO> listBeers() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listBeers'");
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+        System.out.println(response.getBody());
+        return null;
     }
 
 }
--- spring-6-rest-template-main/src/test/java/guru/springframework/spring6resttemplate/clients/BeerClientImplTest.java
+++ spring-6-rest-template-main/src/test/java/guru/springframework/spring6resttemplate/clients/BeerClientImplTest.java
@@ -1,10 +1,16 @@
 package guru.springframework.spring6resttemplate.clients;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class BeerClientImplTest {
+    @Autowired
+    BeerClient beerClientImpl;
+
     @Test
     void testListBeers() {
-
+        beerClientImpl.listBeers();
     }
 }
