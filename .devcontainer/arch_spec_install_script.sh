36290ee195be6ac1ac7615b3713e0f51c1f2d198
prateek <prateekjoshi2013@gmail.com>
2024-07-27 17:42:51 -0700

    Using alpine linux with smaller size had to change install instructions since it uses apk instead of apt by default has root user and no other user

--- .devcontainer/arch_spec_install_script.sh
+++ .devcontainer/arch_spec_install_script.sh
@@ -3,17 +3,17 @@
 # Detect architecture
 ARCH=$(uname -m)
 
-apt-get install -y openjdk-17-jdk
+apk add --update openjdk17
 
 if [ "$ARCH" = "x86_64" ]; then
     echo "Running on x86_64 architecture"
     # Commands for x86_64 architecture
     # Set JAVA_HOME and update PATH
-    echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64" >> ~/.bashrc
+    echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk" >> ~/.bashrc
 elif [ "$ARCH" = "arm64" ] || [ "$ARCH" = "aarch64" ]; then
     echo "Running on arm64 architecture"
     # Commands for arm64/aarch64 architecture
-    echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64" >> ~/.bashrc
+    echo "export JAVA_HOME=/usr/lib/jvm/java-17-openjdk" >> ~/.bashrc
 else
     echo "Unknown architecture: $ARCH"
     exit 1
