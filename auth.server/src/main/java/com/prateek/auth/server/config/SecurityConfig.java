31e1298103720557d4b22ca8952bd65ca06b94ae
root <prateekjoshi2013@gmail.com>
2024-08-04 14:23:30 -0700

    Set up filter chain to expose oauth endpoints this will allow oauth endpoints to be treated specially

---
+++ auth.server/src/main/java/com/prateek/auth/server/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package main.java.com.prateek.auth.server.config;
+
+import java.beans.Customizer;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults()); // Enable OpenID Connect 1.0
+        http
+                // Redirect to the login page when not authenticated from the
+                // authorization endpoint
+                .exceptionHandling((exceptions) -> exceptions
+                        .defaultAuthenticationEntryPointFor(
+                                new LoginUrlAuthenticationEntryPoint("/login"),
+                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)))
+                // Accept access tokens for User Info and/or Client Registration
+                .oauth2ResourceServer((resourceServer) -> resourceServer
+                        .jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+}
