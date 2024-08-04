01d284932892cee56d202b109c7ca50262848d12
root <prateekjoshi2013@gmail.com>
2024-08-04 01:56:53 -0700

    Added proper security configuration by adding csrf ignoring we removed httpbasic authentication as well added httpbasic config and authorize all requests

--- spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/config/SpringSecConfig.java
+++ spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/config/SpringSecConfig.java
@@ -4,16 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
-        http.csrf(httpSecurityCsrfConfigurer -> {
-            httpSecurityCsrfConfigurer.ignoringRequestMatchers("/api/**");
-        });
+    SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+        http
+        // Enable HTTP Basic authentication with default settings
+        .httpBasic(withDefaults())
+        // Configure CSRF to ignore specific paths
+        .csrf(csrf -> csrf
+            .ignoringRequestMatchers("/api/**") // Paths to ignore CSRF protection
+        )
+        // Authorize requests
+        .authorizeHttpRequests(authorizeRequests -> 
+            authorizeRequests
+                .anyRequest().authenticated() // Require authentication for all requests
+        );
+
         return http.build();
+
     }
     
 }
