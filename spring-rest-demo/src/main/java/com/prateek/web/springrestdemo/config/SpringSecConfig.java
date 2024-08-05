458389fb188ed6bcb793a8511383b024d272edcf
root <prateekjoshi2013@gmail.com>
2024-08-04 17:18:46 -0700

    Added Spring Security Config for oauth2 authentication Added Security Filter chain with oauth2ResourceServer Added oauth2 authorization server uri

--- spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/config/SpringSecConfig.java
+++ spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/config/SpringSecConfig.java
@@ -2,30 +2,24 @@ package com.prateek.web.springrestdemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecConfig {
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-        // Enable HTTP Basic authentication with default settings
-        .httpBasic(withDefaults())
-        // Configure CSRF to ignore specific paths
-        .csrf(csrf -> csrf
-            .ignoringRequestMatchers("/api/**") // Paths to ignore CSRF protection
+        .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+            .anyRequest().authenticated()
         )
-        // Authorize requests
-        .authorizeHttpRequests(authorizeRequests -> 
-            authorizeRequests
-                .anyRequest().authenticated() // Require authentication for all requests
+        .oauth2ResourceServer(oauth2 -> oauth2
+            .jwt(Customizer.withDefaults())
         );
-
         return http.build();
 
     }
-    
+
 }
