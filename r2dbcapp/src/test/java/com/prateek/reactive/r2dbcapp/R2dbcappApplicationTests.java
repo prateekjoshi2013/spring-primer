406f756560c80d2070bcf246d6951e33939a90b1
root <prateekjoshi2013@gmail.com>
2024-08-07 17:19:09 -0700

    Initial commit

---
+++ r2dbcapp/src/test/java/com/prateek/reactive/r2dbcapp/R2dbcappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prateek.reactive.r2dbcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
