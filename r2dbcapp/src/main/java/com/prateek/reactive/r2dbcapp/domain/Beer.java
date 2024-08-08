c9cefbe1410725038373fe151883539a005cc861
root <prateekjoshi2013@gmail.com>
2024-08-07 18:57:36 -0700

    Beer Entity for r2dbc persistence

---
+++ r2dbcapp/src/main/java/com/prateek/reactive/r2dbcapp/domain/Beer.java
@@ -0,0 +1,27 @@
+package com.prateek.reactive.r2dbcapp.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Beer {
+    @Id
+    private Integer id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
