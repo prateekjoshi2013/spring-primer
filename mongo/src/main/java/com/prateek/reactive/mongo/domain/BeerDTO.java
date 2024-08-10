feac6c5603bbfa3dc8fb3962b63e40dc16e1d4af
root <prateekjoshi2013@gmail.com>
2024-08-09 19:19:22 -0700

    Added beer model,beer dto and BeerMapper

---
+++ mongo/src/main/java/com/prateek/reactive/mongo/domain/BeerDTO.java
@@ -0,0 +1,24 @@
+package com.prateek.reactive.mongo.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BeerDTO {
+    private String id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
---
+++ mongo/src/main/java/com/prateek/reactive/mongo/model/Beer.java
@@ -0,0 +1,29 @@
+package com.prateek.reactive.mongo.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document
+public class Beer {
+    @Id
+    private String id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
