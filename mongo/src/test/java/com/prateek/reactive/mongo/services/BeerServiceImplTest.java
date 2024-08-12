30d8f58fe17fcc338c15bf1071a11e8fcb0ad055
prateek <prateekjoshi2013@gmail.com>
2024-08-11 19:26:28 -0700

    Added tests for all crud operation

--- mongo/src/test/java/com/prateek/reactive/mongo/services/BeerServiceImplTest.java
+++ mongo/src/test/java/com/prateek/reactive/mongo/services/BeerServiceImplTest.java
@@ -7,8 +7,11 @@ import java.math.BigDecimal;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.prateek.reactive.mongo.config.AbstractTestContainer;
@@ -18,8 +21,10 @@ import com.prateek.reactive.mongo.model.Beer;
 import com.prateek.reactive.mongo.repositories.BeerRepository;
 
 import lombok.SneakyThrows;
+import reactor.core.publisher.Mono;
 
-
+// Test do not support @Transactional so we need to run the tests in a particular order since it shares the context
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BeerServiceImplTest extends AbstractTestContainer {
 
@@ -57,6 +62,7 @@ public class BeerServiceImplTest extends AbstractTestContainer {
                 .build();
     }
 
+    @Order(1)
     @Test
     @SneakyThrows
     void testSaveBeer() {
@@ -71,6 +77,7 @@ public class BeerServiceImplTest extends AbstractTestContainer {
         await().untilTrue(finishedSaving); // if we dont awit here mongo connection is closed before
     }
 
+    @Order(2)
     @Test
     void testFindById() {
         AtomicBoolean finishedSaving = new AtomicBoolean(false);
@@ -81,6 +88,46 @@ public class BeerServiceImplTest extends AbstractTestContainer {
 
         });
         await().untilTrue(finishedSaving); // if we dont awit here mongo connection is closed before
+    }
+
+    @Order(3)
+    @Test
+    void testListBeers() {
+        AtomicBoolean finishedFetching = new AtomicBoolean(false);
+        beerService.listBeers().collectList().subscribe(beers -> {
+            System.out.println(beers);
+            assertThat(beers.size()).isEqualTo(2);
+            finishedFetching.set(true);
+
+        });
+        await().untilTrue(finishedFetching); // if we dont awit here mongo connection is closed before
+    }
+
+    @Order(4)
+    @Test
+    void testUpdateBeer() {
+        AtomicBoolean finishedUpdating = new AtomicBoolean(false);
+        BeerDTO beerDto = getTestBeer();
+        String beerName = "Updated BeerName";
+        beerDto.setBeerName(beerName);
+        beerService.updateBeer(savedBeerId, beerDto).subscribe(updatedBeer -> {
+            System.out.println(updatedBeer);
+            assertThat(updatedBeer.getId()).isEqualTo(savedBeerId);
+            assertThat(updatedBeer.getBeerName()).isEqualTo(beerName);
+            finishedUpdating.set(true);
+
+        });
+        await().untilTrue(finishedUpdating); // if we dont awit here mongo connection is closed before
+    }
+
+    @Order(5)
+    @Test
+    void testDeleteBeer() {
+        AtomicBoolean finishedDeleting = new AtomicBoolean(false);
+        beerService.deleteBeer(savedBeerId).doOnSuccess(unused -> {
+            finishedDeleting.set(true);
+        }).subscribe();
+        await().untilTrue(finishedDeleting); // if we dont await here mongo connection is closed before
     }
 
 }
