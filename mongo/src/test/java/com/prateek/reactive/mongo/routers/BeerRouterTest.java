8f634e6c6203d882297155691b9027974d89a1a3
prateek <prateekjoshi2013@gmail.com>
2024-08-12 00:49:15 -0700

    Added Router test for get beer by id

--- mongo/src/test/java/com/prateek/reactive/mongo/routers/BeerRouterTest.java
+++ mongo/src/test/java/com/prateek/reactive/mongo/routers/BeerRouterTest.java
@@ -3,12 +3,21 @@ package com.prateek.reactive.mongo.routers;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.prateek.reactive.mongo.domain.BeerDTO;
+import com.prateek.reactive.mongo.services.BeerServiceImplTest;
+
+import reactor.core.publisher.Mono;
 
 @ActiveProfiles("default")
 @SpringBootTest
@@ -18,9 +27,40 @@ public class BeerRouterTest {
     WebTestClient webTestClient;
 
     @Test
-    void testBeerRoutes() {
+    void testListBeerRoute() {
         webTestClient.get().uri(BeerRouter.BEER_PATH).exchange().expectStatus().isOk()
                 .expectHeader().valueEquals("Content-type", "application/json")
                 .expectBody().jsonPath("$.size()", hasSize(greaterThan(1)));
+    }
+
+    @Test
+    void testGetBeerByIdRoute() {
+        BeerDTO savedBeer = getSavedTestBeer();
+        webTestClient.get().uri(BeerRouter.BEER_PATH_ID, savedBeer.getId()).exchange().expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody().jsonPath("$.size()", hasSize(greaterThan(1)));
+    }
+
+    public BeerDTO getSavedTestBeer() {
+        FluxExchangeResult<BeerDTO> beerDTOFluxExchangeResult = webTestClient.post().uri(BeerRouter.BEER_PATH)
+                .body(Mono.just(getTestBeer()), BeerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .returnResult(BeerDTO.class);
+
+        List<String> location = beerDTOFluxExchangeResult.getResponseHeaders().get("Location");
+
+        return webTestClient.get().uri(BeerRouter.BEER_PATH)
+                .exchange().returnResult(BeerDTO.class).getResponseBody().blockFirst();
+    }
+
+    public BeerDTO getTestBeer() {
+        return BeerDTO.builder()
+                .beerName("My Bear")
+                .beerStyle("PALE_ALE")
+                .price(BigDecimal.TEN)
+                .quantityOnHand(10)
+                .upc("upc")
+                .build();
     }
 }
