c17c8fe75e489958fcea232ee214f87ee2c49b1d
prateek <prateekjoshi2013@gmail.com>
2024-08-11 19:26:05 -0700

    Added the rest of CRUD operation in service

--- mongo/src/main/java/com/prateek/reactive/mongo/services/BeerService.java
+++ mongo/src/main/java/com/prateek/reactive/mongo/services/BeerService.java
@@ -2,10 +2,17 @@ package com.prateek.reactive.mongo.services;
 
 import com.prateek.reactive.mongo.domain.BeerDTO;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface BeerService {
     Mono<BeerDTO> saveBeer(BeerDTO beerDTO);
 
     Mono<BeerDTO> findById(String beerId);
+
+    Flux<BeerDTO> listBeers();
+
+    Mono<BeerDTO> updateBeer(String beerId, BeerDTO beer);
+    
+    Mono<Void> deleteBeer(String beerId);
 }
--- mongo/src/main/java/com/prateek/reactive/mongo/services/BeerServiceImpl.java
+++ mongo/src/main/java/com/prateek/reactive/mongo/services/BeerServiceImpl.java
@@ -1,13 +1,20 @@
 package com.prateek.reactive.mongo.services;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.prateek.reactive.mongo.domain.BeerDTO;
 import com.prateek.reactive.mongo.mappers.BeerMapper;
 import com.prateek.reactive.mongo.repositories.BeerRepository;
 
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -29,6 +36,36 @@ public class BeerServiceImpl implements BeerService {
         return beerRepository
                 .save(beerMapper.beerDTOtoBeer(beerDTO))
                 .map(beerMapper::beerToBeerDTO);
+    }
+
+    @Override
+    public Flux<BeerDTO> listBeers() {
+        return beerRepository
+                .findAll()
+                .map(beerMapper::beerToBeerDTO);
+    }
+
+    @Override
+    public Mono<BeerDTO> updateBeer(String beerId, BeerDTO beerDTO) {
+        return beerRepository
+                .findById(beerId)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Beer not found")))
+                .flatMap(savedBeer -> {
+                    savedBeer.setBeerName(beerDTO.getBeerName());
+                    savedBeer.setBeerStyle(beerDTO.getBeerStyle());
+                    savedBeer.setPrice(beerDTO.getPrice());
+                    savedBeer.setQuantityOnHand(beerDTO.getQuantityOnHand());
+                    savedBeer.setUpc(beerDTO.getUpc());
+                    return beerRepository.save(savedBeer).map(beerMapper::beerToBeerDTO);
+                });
+    }
+
+    @Override
+    public Mono<Void> deleteBeer(String beerId) {
+        return beerRepository
+                .findById(beerId)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Beer not found")))
+                .then(Mono.empty());
     }
 
 }
