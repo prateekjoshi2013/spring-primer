e8f77ff8597109ab80ff1628bfaa58da4ddcbdd6
prateek <prateekjoshi2013@gmail.com>
2024-08-11 23:03:45 -0700

    Added BeerHandler

---
+++ mongo/src/main/java/com/prateek/reactive/mongo/handlers/BeerHandler.java
@@ -0,0 +1,23 @@
+package com.prateek.reactive.mongo.handlers;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.prateek.reactive.mongo.domain.BeerDTO;
+import com.prateek.reactive.mongo.services.BeerService;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeers(ServerRequest request) {
+        return ServerResponse
+                .ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+}
