7afc76619f66614f359c64e48396de072a0ad8ce
prateek <prateekjoshi2013@gmail.com>
2024-08-16 16:48:50 -0700

    Added SecurityFilterChain for actuator endpoints

--- spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/config/SpringSecConfig.java
+++ spring-rest-demo/src/main/java/com/prateek/web/springrestdemo/config/SpringSecConfig.java
@@ -1,8 +1,10 @@
 package com.prateek.web.springrestdemo.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +14,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecConfig {
 
     @Bean
+    @Order(1)
+    SecurityFilterChain actuatoSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll());
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
