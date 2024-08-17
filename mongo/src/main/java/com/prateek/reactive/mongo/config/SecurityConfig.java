434608b40e79afbaa131d392309c0b045bda95cb
prateek <prateekjoshi2013@gmail.com>
2024-08-17 11:27:41 -0700

    Added kubernetes actuator endpoints to webflux.fn app

--- mongo/src/main/java/com/prateek/reactive/mongo/config/SecurityConfig.java
+++ mongo/src/main/java/com/prateek/reactive/mongo/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.prateek.reactive.mongo.config;
 
+
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -10,7 +13,15 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+    @Bean
+    @Order(1)
+    SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(authorize -> authorize.anyExchange().permitAll());
+        return http.build();
+    }
 
+    @Order(2)
     @Bean
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
